9e211be474741411af187d787986822b49134785
DESKTOP-FQO43D2\Home <truonghuong240928@gmail.com>
2022-11-24 07:59:45 +0700

    no message

--- src/main/java/services/impl/CauHinhServiceImpl.java
+++ src/main/java/services/impl/CauHinhServiceImpl.java
@@ -4,10 +4,58 @@
  */
 package services.impl;
 
+import domainmodels.CauHinh;
+import infrastructure.convert.FormUtil;
+import java.util.ArrayList;
+import java.util.List;
+import reponces.QlCauHinh;
+import responsitiories.impl.CauHinhReponsitoryImpl;
+import services.IService;
+
 /**
  *
  * @author Admin
  */
-public class CauHinhServiceImpl {
+public class CauHinhServiceImpl implements IService<QlCauHinh>{
+
+    private CauHinhReponsitoryImpl cauHinhReponsitoryImpl;
+    private List<QlCauHinh> qlCauHinhs;
+    
+    public CauHinhServiceImpl(){
+        this.cauHinhReponsitoryImpl = new CauHinhReponsitoryImpl();
+    }
+    @Override
+    public List<QlCauHinh> findAll() {
+        this.qlCauHinhs = new ArrayList<>();
+        this.cauHinhReponsitoryImpl.findAll().forEach(cauHinh -> {
+            qlCauHinhs.add(new QlCauHinh(cauHinh.getId(), cauHinh.getIdManHinh(), cauHinh.getIdRam(), cauHinh.getIdRom()));
+        });
+        return qlCauHinhs;
+    }
+
+    @Override
+    public QlCauHinh findOne(String ma) {
+        CauHinh cauHinh = this.cauHinhReponsitoryImpl.findOneByMa(ma);
+        return FormUtil.convertQlCauHinhToCauHinh(cauHinh);
+    }
+
+    @Override
+    public String save(QlCauHinh qlCauHinh) {
+        CauHinh cauHinh = FormUtil.convertCauHinhToQLCauHinh(qlCauHinh);
+       return this.cauHinhReponsitoryImpl.save(cauHinh)== true? "Thêm thành công":"Thêm thất bại";
+    }
+
+    @Override
+    public String delete(QlCauHinh t) {
+       CauHinh cauHinh = FormUtil.convertCauHinhToQLCauHinh(t);
+        boolean result = this.cauHinhReponsitoryImpl.delete(cauHinh);
+        return  result==true?"Xóa thành công":"Xóa thất bại ";
+    }
+
+    @Override
+    public String update(QlCauHinh t) {
+       CauHinh cauHinh = FormUtil.convertCauHinhToQLCauHinh(qlCauHinh));
+       return this.cauHinhReponsitoryImpl.update(cauHinh) == true?"update thành công":"update thất bại ";
+    }
     
 }
--- src/main/java/services/impl/ManHinhServiceImpl.java
+++ src/main/java/services/impl/ManHinhServiceImpl.java
@@ -4,10 +4,59 @@
  */
 package services.impl;
 
+import domainmodels.ManHinh;
+import infrastructure.convert.FormUtil;
+import java.util.ArrayList;
+import java.util.List;
+import reponces.QlManHinh;
+import reponces.QlMauSac;
+import responsitiories.impl.ManHinhRepository;
+import services.IService;
+
 /**
  *
  * @author Admin
  */
-public class ManHinhServiceImpl {
-    
+public class ManHinhServiceImpl implements IService<QlManHinh>{
+    private ManHinhRepository manHinhReponsitoryImpl;
+    private List<QlManHinh> qlManHinhs;
+
+    public ManHinhServiceImpl() {
+        this.manHinhReponsitoryImpl = new ManHinhRepository();
+    }
+
+    @Override
+    public List<QlManHinh> findAll() {
+        qlManHinhs = new ArrayList<>();
+        this.manHinhReponsitoryImpl.findAll().forEach(manHinh -> {
+            qlManHinhs.add(FormUtil.convertQlManHinhToManHinh(manHinh));
+        });
+        return qlManHinhs;
+    }
+
+    @Override
+    public QlManHinh findOne(String ma) {
+        ManHinh manHinh = this.manHinhReponsitoryImpl.findOneByMa(ma);
+        return FormUtil.convertQlManHinhToManHinh(manHinh);
+    }
+
+    @Override
+    public String save(QlManHinh qlManHinh) {
+        ManHinh manHinh = FormUtil.convertManHinhToQLManHinh(qlManHinh);
+        return this.manHinhReponsitoryImpl.save(manHinh)==true?"Thêm thành công":"Thêm thất bại";
+    }
+
+    @Override
+    public String delete(QlManHinh qlManHinh) {
+        ManHinh manHinh = FormUtil.convertManHinhToQLManHinh(qlManHinh);
+        boolean result = this.manHinhReponsitoryImpl.delete(manHinh);
+        return  result==true?"Xóa thành công":"Xóa thất bại ";
+    }
+
+    @Override
+    public String update( QlManHinh qlManHinh) {
+        ManHinh manHinh  = FormUtil.convertManHinhToQLManHinh(qlManHinh);
+        return this.manHinhReponsitoryImpl.update(manHinh)==true?"update thành công":"update thất bại ";
+    }
+
 }
